a43b271b17e2aac6637016f8fdaf0f60792e14fb
MuzakirAhamed <muzakirahmed97@gmail.com>
2023-09-14 09:43:41 +0530

    created home page route

--- routes/dataroutes.js
+++ routes/dataroutes.js
@@ -1,18 +1,17 @@
 const Locations = require("../controllers/locationsController");
-const Restaurants = require('../controllers/restaurantsController')
-const Mealtypes = require('../controllers/mealtypesController')
-const Menu = require('../controllers/menuController')
+const Restaurants = require("../controllers/restaurantsController");
+const Mealtypes = require("../controllers/mealtypesController");
+const Menu = require("../controllers/menuController");
 const express = require("express");
 const router = express.Router();
-
+router.route("/").get((req, res) => res.send('<h1>Welcome!!</h1>'));
 router.route("/locations").get(Locations.getAllLocations);
 router.route("/mealtypes").get(Mealtypes.getAllMealtypes);
 router.route("/restaurants").get(Restaurants.getAllrestaurant);
-router.route("/menu").get(Menu.getMenu)
-router.route("/restaurants/:id").get(Restaurants.getRestaurant)
-router.route("/restaurantdetails").get(Restaurants.getRestaurantdetails)
-router.route("/mealtyperestaurant").get(Restaurants.getRestaurantmealtype)
-router.route("/filteredValue").get(Restaurants.getFilteredValue)
-// router.route("/minPricerestaurant").get(Restaurants.getMinPriceRestaurants)
+router.route("/menu").get(Menu.getMenu);
+router.route("/restaurants/:id").get(Restaurants.getRestaurant);
+router.route("/restaurantdetails").get(Restaurants.getRestaurantdetails);
+router.route("/mealtyperestaurant").get(Restaurants.getRestaurantmealtype);
+router.route("/filteredValue").get(Restaurants.getFilteredValue);
 
 module.exports = router;
